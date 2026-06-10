eb35be9384653e847f2944e52d977d4421108ee5
Linco <llincolnolim@gmail.com>
2026-06-10 10:13:11 -0300

    Versão 1.8.1

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 import bcrypt
 import mysql.connector
-
 app = Flask(__name__)
 app.secret_key = "chave_secreta"
 
